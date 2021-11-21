b2b6520a6b8a072a3d9a9b175e653454d8e11996
ArnoldOlanda <olanda188@gmail.com>
2021-11-21 09:48:31 -0500

    Subido codigo fuente de la practica

---
+++ Ejercicio_2/Ejercicio_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ejercicio_2.Startup))]
+namespace Ejercicio_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
